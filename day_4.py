b29333e26950240ca6581be7adab5aef63386a8e
Greg Amato <amatobahn@gmail.com>
2018-12-04 19:54:02 -0600

    Completed Day 4. Definitely utilized collections.Counter() for this solve.

---
+++ day_4.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+--- Day 4: Repose Record ---
+
+-- Part One --
+You've sneaked into another supply closet - this time, it's across from the prototype suit manufacturing lab.
+You need to sneak inside and fix the issues with the suit, but there's a guard stationed outside the lab, so
+this is as close as you can safely get.
+
+As you search the closet for anything that might help, you discover that you're not the first person to want
+to sneak in. Covering the walls, someone has spent an hour starting every midnight for the past few months
+secretly observing this guard post! They've been writing down the ID of the one guard on duty that night
+the Elves seem to have decided that one guard was enough for the overnight shift - as well as when they fall
+asleep or wake up while at their post (your puzzle input).
+
+For example, consider the following records, which have already been organized into chronological order:
+
+[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up
+
+Timestamps are written using year-month-day hour:minute format. The guard falling asleep or waking up is always
+the one whose shift most recently started. Because all asleep/awake times are during the midnight hour (00:00 - 00:59),
+only the minute portion (00 - 59) is relevant for those events.
+
+Visually, these records show that the guards are asleep at these times:
+
+Date   ID   Minute
+            000000000011111111112222222222333333333344444444445555555555
+            012345678901234567890123456789012345678901234567890123456789
+11-01  #10  .....####################.....#########################.....
+11-02  #99  ........................................##########..........
+11-03  #10  ........................#####...............................
+11-04  #99  ....................................##########..............
+11-05  #99  .............................................##########.....
+
+The columns are Date, which shows the month-day portion of the relevant day; ID, which shows the guard on
+duty that day; and Minute, which shows the minutes during which the guard was asleep within the midnight hour.
+(The Minute column's header shows the minute's ten's digit in the first row and the one's digit in the second row.)
+Awake is shown as ., and asleep is shown as #.
+
+Note that guards count as asleep on the minute they fall asleep, and they count as awake on the minute they wake up.
+For example, because Guard #10 wakes up at 00:25 on 1518-11-01, minute 25 is marked as awake.
+
+If you can figure out the guard most likely to be asleep at a specific time, you might be able to trick that guard
+into working tonight so you can have the best chance of sneaking in. You have two strategies for choosing the best
+guard/minute combination.
+
+Strategy 1: Find the guard that has the most minutes asleep. What minute does that guard spend asleep the most?
+
+In the example above, Guard #10 spent the most minutes asleep, a total of 50 minutes (20+25+5), while Guard #99
+only slept for a total of 30 minutes (10+10+10). Guard #10 was asleep most during minute 24 (on two days, whereas
+any other minute the guard was asleep was only seen on one day).
+
+While this example listed the entries in chronological order, your entries are in the order you found them.
+You'll need to organize them before they can be analyzed.
+
+What is the ID of the guard you chose multiplied by the minute you chose?
+(In the above example, the answer would be 10 * 24 = 240.)
+
+--- Part Two ---
+
+Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
+
+In the example above, Guard #99 spent minute 45 asleep more than any other guard or minute - three times in total.
+(In all other cases, any guard spent any minute asleep at most twice.)
+
+What is the ID of the guard you chose multiplied by the minute you chose?
+(In the above example, the answer would be 99 * 45 = 4455.)
+
+"""
+
+from collections import Counter, defaultdict
+from datetime import datetime
+
+
+def generate_data_from_log(log_data: list) -> defaultdict(Counter):
+    """
+    Formats data to parse info from
+
+    Arguments:
+        log_data (list): List of unordered log entries for guard sleep and wake states
+    """
+    _guards = defaultdict(Counter)
+
+    guard = 0
+    sleep_start = datetime.now()
+
+    for time, action in [entry.split('] ') for entry in sorted(log_data) if entry]:
+
+        time = datetime.strptime(time, '[%Y-%m-%d %H:%M')
+        # Isolate guard identification from string input
+        if '#' in action:
+            guard = int(action.split('#')[1].split()[0])
+
+        # Get the sleep start time for the guard
+        if 'asleep' in action:
+            sleep_start = time
+
+        # Get the wake up time span from moment guard fell asleep to waking up.
+        # Update the guard's counter for sleep state
+        if 'wakes' in action:
+            min_asleep = int((time - sleep_start).total_seconds() // 60)
+            _guards[guard].update(Counter((sleep_start.minute + i) % 60 for i in range(min_asleep)))
+
+    return _guards
+
+
+def find_worst_guard(guards: defaultdict(Counter)):
+    """
+    Finds the guard who should be first on the chopping block based on total number of minutes slept
+    while on the job during the midnight hour
+
+    Arguments:
+        guards (defaultdict(Counter)): Generated data log of all guards on duty and time spent sleeping
+    """
+
+    worst_guard = max((sum(guard.values()), identification) for identification, guard in guards.items())[1]
+    return worst_guard * guards[worst_guard].most_common()[0][0]
+
+
+def find_most_consistent_guard(guards: defaultdict(Counter)):
+    """
+    Finds the guard who frequently sleeps during the same minute across all days the guard has a shift.
+
+    Arguments:
+        guards (defaultdict(Counter)): Generated data log of all guards on duty and time spent sleeping
+    """
+
+    (_, m), consistent_guard = max((guard.most_common()[0][::-1], identification) for identification, guard in guards.items())
+    return consistent_guard * m
+
+
+if __name__ == "__main__":
+
+    # Grab data input from text file
+    data_log = open("puzzle_inputs/input_day_4.txt", "r").readlines()
+
+    # Generate guard data
+    guard_data = generate_data_from_log(data_log)
+
+    # Get results of the two guards who should be canned
+    print(find_worst_guard(guard_data), find_most_consistent_guard(guard_data))
